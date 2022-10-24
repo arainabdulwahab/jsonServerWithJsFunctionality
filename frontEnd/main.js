14083520d59f4c20ee9d2175f0d722a5fb20b8af
arainabdulwahab <arainabdulwahab6@gmail.com>
2022-10-24 16:43:56 +0200

    search item js start

--- frontEnd/main.js
+++ frontEnd/main.js
@@ -1,43 +1,11 @@
-// background change
-let userBackGrButton = document.getElementById('backGrSelect');
+const inputAdd = document.getElementById('search-add');
+const addButton = document.getElementById('Addbutton');
+addButton.addEventListener('click', () => {
 
-// fetch user data 
-fetch('http://localhost:3000/backgroundImage')
-.then((response) => response.json())
-.then(json => {
-    json.map(data =>{
-        userBackGrButton.append(backGrBtn(data));
-    });
 });
-// background button 
-function backGrBtn(data){
-    let div = document.createElement('div');
-    div.innerHTML = `
-    <div class="dropdown">
-      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-        select background image
-      </button>
-      <ul class="dropdown-menu " aria-labelledby="dropdownMenuButton1">
-        <li class="d-inline">
-          <img src="${data.image1}" class="img-fluid img-thumbnail" alt="..." width="20%" height="auto">
-        </li>
-        <li class="d-inline">
-          <img src="${data.image2}" class="img-thumbnail" alt="..." width="20%" height="auto">
-        </li>
-        <li class="d-inline">
-          <img src="${data.image3}" class="img-thumbnail" alt="..." width="20%" height="auto">
-        </li>
-        <li class="d-inline">
-          <img src="${data.image4}" class="img-thumbnail" alt="..." width="20%" height="auto">
-        </li>
-        <li class="d-inline">
-          <img src="${data.image5}" class="img-thumbnail" alt="..." width="20%" height="auto">
-        </li>
-      </ul>
-    </div>
-    `;
-    return div;
-}
 
 
 
+fetch("http://localhost:3000/search")
+.then(res => res.json())
+//.then(data =>{})
