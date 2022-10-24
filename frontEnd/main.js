9ba76787fc9e4982db04f929ed0d7c52a50df3e7
arainabdulwahab <arainabdulwahab6@gmail.com>
2022-10-25 00:14:03 +0200

    search idividual item

--- frontEnd/main.js
+++ frontEnd/main.js
@@ -1,20 +1,60 @@
 const inputAdd = document.getElementById('search-add');
 const addButton = document.getElementById('Addbutton');
+let searchImg = 'broccoli';
+
+
+// search state.json and filter it 
+const searchStates = async searchText =>{ 
+  const res = await fetch("http://localhost:3000/search");
+  const states = await res.json();
+
+
+    // get matches to current text input 
+    let matches = states.filter(state => {
+      const regex = new RegExp(`^${searchText}`, 'gi');
+      return state.name.match(regex);
+    });
+    // empty search box 
+    if(searchText.length === 0){ matches = []; matchList.innerHTML = ''}
+    //console.log(matches)
+    outputHtml(matches);
+  };
+// show result in html 
+const outputHtml = matches => {
+  if(matches.length > 0 ){
+    const htmls = matches.map(match => `
+      <div>
+        <img src="${match.image}" width="40"><h1>${match.name} </h1>
+      </div>
+    `).join('');
+    matchList.innerHTML = htmls;
+  } 
+
+}
+
+
+
+
+
 addButton.addEventListener('click', () => {
   // delete button
   let deleteButton = document.createElement('button');
   deleteButton.innerHTML = 'delete';
   deleteButton.id = 'done';
 
+ 
+  // image
+  let imageSearch = document.createElement('img');
+  imageSearch.src = 'img/'+ searchImg +'.jpg'; 
+  imageSearch.width = '40';
   // input 
   let inputItem = document.createElement('input');
   inputItem.type = 'search';
   inputItem.placeholder = 'noe';
 
-  // image
-  let imageSearch = document.createElement('img');
-  imageSearch.src = 'img/broccoli.jpg'; 
-  imageSearch.width = '40';
+  // match list div 
+  let matchList = document.createElement('div');
+  matchList.id = 'matchList'
   // bar added 
   let bar = document.createElement('br');
 
@@ -23,22 +63,16 @@ addButton.addEventListener('click', () => {
   checkbox.value = "value";
   checkbox.id = "id";
 
-  
+
   // append all item
-  inputAdd.append(deleteButton,  imageSearch, inputItem, checkbox, bar);
+  inputAdd.append(deleteButton,  imageSearch, inputItem, checkbox,matchList, bar);
 
   deleteButton.addEventListener('click', () => {
     console.log('delete done');
   });
 
-  inputItem.addEventListener('keyup', () => {
-    console.log(inputItem.value);
-
-  });
+  inputItem.addEventListener('input', () => searchStates(inputItem.value));
+  console.log();
 });
 
 
-
-// fetch("http://localhost:3000/search")
-// .then(res => res.json())
-// //.then(data =>{})
