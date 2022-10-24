db4df6e258e80d579c2a32b3f0ec735feea3243d
arainabdulwahab <arainabdulwahab6@gmail.com>
2022-10-24 17:07:32 +0200

    add button functional

--- frontEnd/main.js
+++ frontEnd/main.js
@@ -1,11 +1,34 @@
 const inputAdd = document.getElementById('search-add');
 const addButton = document.getElementById('Addbutton');
 addButton.addEventListener('click', () => {
+  // delete button
+  let deleteButton = document.createElement('button');
+  deleteButton.innerHTML = 'delete';
+  deleteButton.id = 'done';
 
+  // input 
+  let inputItem = document.createElement('input');
+  inputItem.type = 'search';
+
+  // image
+  let imageSearch = document.createElement('image');
+  //imageSearch.src = 'frontEnd/img/broccoli.jpa'; 
+
+  // append all item
+  inputAdd.append(deleteButton, inputItem, imageSearch);
+
+  deleteButton.addEventListener('click', () => {
+    console.log('delete done');
+  });
+
+  inputItem.addEventListener('keyup', () => {
+    console.log(inputItem.value);
+
+  });
 });
 
 
 
-fetch("http://localhost:3000/search")
-.then(res => res.json())
-//.then(data =>{})
+// fetch("http://localhost:3000/search")
+// .then(res => res.json())
+// //.then(data =>{})
